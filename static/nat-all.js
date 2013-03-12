52e88fcd2b44958e28575c79e5014c11012b76b4
ultra2 <a@a.hu>
2013-03-12 21:22:19 +0100

    commit

--- static/nat-all.js
+++ static/nat-all.js
@@ -4042,6 +4042,8 @@ debugger;
 
         for (var i=0; stores.length>i; i++){
             var store = stores[i];
+            store.collection = store.model;
+            delete store.model;
             store = Ext.create('widget.' + store.xtype, store);
             store.collection = store.model;
             me.stores.add(store);
